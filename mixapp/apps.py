e910e6b1a39f59bbdcf116e7f36c1e263be29092
santhusanthosh001 <sallurisanthosh46@gmail.com>
2023-10-10 11:07:22 +0530

    first

---
+++ mixapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MixappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mixapp'
---
+++ mixapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class Regform(forms.Form):
+
+    First_name = forms.CharField(max_length=100)
+    Last_name = forms.CharField(max_length=100)
+    user_name = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=10,widget=forms.PasswordInput())
+    cpassword = forms.CharField(max_length=10,widget=forms.PasswordInput())
+    emailid = forms.EmailField()
+    mobilenumber = forms.IntegerField()
+class loginform(forms.Form):
+
+    user_name=forms.CharField(max_length=100)
+    password=forms.CharField(max_length=10,widget=forms.PasswordInput())
+
